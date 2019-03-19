e11b557f486d22c3d1aa15d792ec56970379165b
sonici <sonici16@naver.com>
2019-03-19 18:13:58 +0900

    no message

---
+++ Programmers/programmers/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "987654321";
+            int k=3;                                // 제외될 자릿수
+            string answer = "";
+
+            Stack<char> stack = new Stack<char>();  //스택
+            int answerLength = number.Length - k;      //answer 자릿수
+            for (int i = 0; i < number.Length; i++)
+            {
+                char ch = number[i];
+
+                for (; (stack.Count !=0) && k > 0; k--)
+                {
+                    char stack_top = stack.Peek();
+                    if (stack_top >= ch)
+                        break;                      //만약 스택 가장 위에있는 수보다 ch가 작거나 같으면 break
+
+                    stack.Pop();                    //처리하고자 하는 수가 더 크다면 스택 가장 위에있는 수를 제거
+                }
+
+                stack.Push(ch);                      
+            }
+
+            while (stack.Count != answerLength)
+                stack.Pop();
+
+
+
+            while (stack.Count != 0)                //answer
+            {
+                char x = stack.Peek();
+                answer = x + answer;
+                stack.Pop();
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Programmers/programmers/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            int [] citations = new int[] { 3, 3, 6, 2, 5, 1, 2, 1 };
+
+            Array.Sort(citations);              // 올림차순
+            Array.Reverse(citations);           // 내림차순 {6,5,3,3,2,2,1,1}
+
+            for (int i = 0; i < citations.Length; i++)
+            {
+                int a = citations[answer];  // 디버깅 용
+                int b = answer + 1;         // 디버깅 용
+                if (answer <= citations[i] && a >= b) //4 //3 // 중복잼
+                    answer++;
+            }
+
+            Console.WriteLine("h-index: " + answer);
+        }
+    }
+}
